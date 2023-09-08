e16aa7ff10b9d349cbdf4388a0820f0cfe713a25
Gabriel <geraldg652@gmail.com>
2023-09-08 12:17:39 +0300

    minor updates

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const moment = require("moment-timezone");
 const app = express();
 
 app.get("/api", (req, res) => {
-  const { slackName, track } = req.query;
+  const { slack_name, track } = req.query;
 
   const currentDay = moment().tz("UTC").format("dddd");
   const utcTime = moment().tz("UTC").format();
 
-  const githubFileUrl = "https://github.com/gabrielgatimu/HNGx-10";
+  const githubFileUrl =
+    "https://github.com/GabrielGatimu/HNGx-10/blob/main/app.js";
   const githubRepoUrl = "https://github.com/gabrielgatimu/HNGx-10";
 
   const response = {
-    slack_name: slackName,
+    slack_name: slack_name,
     current_day: currentDay,
-    utcTime: utcTime,
+    utc_time: utcTime,
     track: track,
     github_file_url: githubFileUrl,
     github_repo_url: githubRepoUrl,
